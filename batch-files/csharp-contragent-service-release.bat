61c4327c0e71a989854e0eadec6687acc19c62e7
Anton Miroshkin <anton.miroshkin@gmail.com>
2017-09-20 16:58:46 +0300

    Add Orders and c sharp  projects.

---
+++ batch-files/csharp-contragent-service-release.bat
@@ -0,0 +1,1 @@
+START "" "c:\tfs\v1.0\Release\V1\V1 Release\AL\Services\Emex.AL.Services.Contragent\Emex.AL.Services.Contragent.sln"
---
+++ batch-files/order-emex-dev.bat
@@ -0,0 +1,1 @@
+START c:\tfs\v1.0\TRC\Dev\NoPDA\WinForms\Old\UserInterface.sln
